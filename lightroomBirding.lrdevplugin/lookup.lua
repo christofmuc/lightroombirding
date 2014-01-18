b5cb37769ac0057956a733e0ef7c2a51086fcb03
christof <christof.ruch@gmx.de>
2014-01-18 12:37:34 +0100

    First implementation of finding out if a photo is tagged with a species name, and then setting it in the birding info metadata

--- lightroomBirding.lrdevplugin/lookup.lua
+++ lightroomBirding.lrdevplugin/lookup.lua
@@ -1,5 +1,4 @@
 -- http://www.johnrellis.com/lightroom/debugging-toolkit.htm
-local Require = require 'Require'.reload()
 local Debug = require'Debug'.init()
 require'strict'
 
@@ -10,11 +9,47 @@ local LrDate = import'LrDate'
 require'alpenglowUtil'
 require'PluginInit'
 
+function isInBirdNameHierarchy(keyword)
+    if keyword:getName() == "Birds" then
+        return true
+    elseif not keyword:getParent() then
+        return false
+    else
+        return isInBirdNameHierarchy(keyword:getParent())
+    end
+end
+
 local lookupSpeciesLibraryItem = {}
 function lookupSpeciesLibraryItem.lookup()
     LrTasks.startAsyncTask(Debug.showErrors(function(context)
+        local catalog = LrApplication.activeCatalog()
+        local selected = catalog:getTargetPhotos()
+
         for key, photo in pairs(selected) do
-            local metadata = photo:getRawMetadata('customMetadata')
+            local keywords = photo:getRawMetadata('keywords')
+            Debug.pause(photo:getRawMetadata('customMetadata'))
+            -- Find out if this photo has a keyword that is part of our species hierarchy
+            local species = {}
+            local n = 1
+            for key, word in pairs(keywords) do
+                if next(word:getChildren()) == nil then
+                    -- have found leaf node keyword, is it a bird?
+                    if isInBirdNameHierarchy(word) then
+                        species[n] = word:getName()
+                        n = n + 1
+                    end
+                end
+            end
+            Debug.pause(species)
+
+            -- If it is, then find out the common and the scientific names of the birds
+            local commonNames = ""
+            local scientificNames = ""
+            for key, s in pairs(species) do
+                catalog:withWriteAccessDo("LightroomBirding copying species information", Debug.showErrors(function(context)
+                    photo:setPropertyForPlugin(_PLUGIN, "commonName", s)
+                end))
+            end
         end
     end))
 end
